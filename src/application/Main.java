82a0335d01c018f0e1569b809afd92807858631a
miguelpan <miguel_tobi@hotmail.com>
2020-05-10 03:00:13 -0300

    Comentando o codigo da class main

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,24 +7,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
+/*
+*Classe básica .
+	*Um a aplicação javaFX tem que ser uma classe que extend Application.
+	*Essa classe prossui um método abstrado que é o start (Metodo que é obrigado ser implementado.
+	*Essa classe possui outros métodos, como o inti e o stop
+	 */
 
 public class Main extends Application {
-	
+
 	private static Scene mainScene;
-		
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			/*primaryStage é o palco.
+			 *mainScene é a cena.
+			 */
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));//Linha responsável por pegar o arquivoFXML referente a cena.
 			ScrollPane scrollPane = loader.load();
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
 			mainScene = new Scene(scrollPane);
-			primaryStage.setScene(mainScene);
+			primaryStage.setScene(mainScene);// Aqui é colocado a cena dentro do palco principal
 			primaryStage.setTitle("Projeto JDBC/FX");
-			primaryStage.show();
-			primaryStage.setResizable(false);// faz com que a janela fique fixa 
+			primaryStage.show();//Mostra na tela o conteúdo do palco(primaryStage).
+			primaryStage.setResizable(false);// faz com que a janela fique sem poder ser redimensionada.
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -33,6 +42,9 @@ public class Main extends Application {
 	public static Scene getMainScene() {
 		return mainScene;
 	}
+	/*
+	 * Onde começa a aplicação
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
