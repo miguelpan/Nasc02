c30121c616095a0d4fc8b72cddb5be0a5110f8d2
miguelpan <miguel_tobi@hotmail.com>
2020-05-20 00:37:25 -0300

    Upgrade na função generica para chamar telas

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerta;
@@ -15,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.FuncaoService;
 
 public class MainViewController implements Initializable {
 	/*
@@ -41,9 +43,15 @@ public class MainViewController implements Initializable {
 	public void onMenuItemFuncionarioAction() {
 		System.out.println("Funcionario");
 	}
+	/**
+	 * Passando o caminho e uma função pra inicializaro controlador.
+	 */
 	@FXML
 	public void onMenuItemFuncaoAction() {
-		loadView("/gui/FuncaoLista.fxml");
+		loadView("/gui/FuncaoLista.fxml",(FuncaoListaController controller) -> {// Tipo e nome
+			controller.setFuncaoService(new FuncaoService());// Chamando inicialização do controler FuncaoListaController
+			controller.atualizarTableView();
+		});
 	}
 	@FXML
 	public void onMenuItemClientesAction() {
@@ -51,15 +59,15 @@ public class MainViewController implements Initializable {
 	}
 	@FXML
 	public void onMenuItemSobreAction() {
-		loadView("/gui/Sobre.fxml");
+		loadView("/gui/Sobre.fxml", x -> {});
 	}
 	
 	/**
-	 * Função para abrir tela.
+	 * Função generica para abrir tela.
 	 * Essa função serve pra pegar o VBox da janela que foi passada como parametro e substituir na mainView principal.
 	 * @param nomeDaView, responsavel por pegar a view que foi passada.
 	 */
-	private synchronized void loadView(String nomeDaView) {
+	private synchronized <T> void loadView(String nomeDaView, Consumer<T> acaoInicializar) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeDaView));// Instanciando o FXMLLOADER para caregar a tela que foi passada como parametro.
 			VBox newBox = loader.load();// Instanciando um obj do tipo VBox recebendo o conteudo do VBox da view que foi passada.
@@ -75,6 +83,12 @@ public class MainViewController implements Initializable {
 			principalVbox.getChildren().clear();//Apagando o conteudo do children do MainView.fxml(janela principal).
 			principalVbox.getChildren().add(menuPrincipal);//Passando o menuPrincipal para a janela principal.
 			principalVbox.getChildren().addAll(newBox.getChildren());// Passando o conteudo do newBox para a janela principal.
+			
+			/*
+			 * Executa a função passada como argumento
+			 */
+			T controller = loader.getController();/// getController retorna o controlador do tipo que foi chamado. 
+			acaoInicializar.accept(controller);
 			
 		}catch (IOException e) {
 			Alerta.showAlert("IO Exception", "Erro ao carregar a view 'Sobre'", e.getMessage(), AlertType.ERROR);//Mostrando um alerta caso ocorra error.	
